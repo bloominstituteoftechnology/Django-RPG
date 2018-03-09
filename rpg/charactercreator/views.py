956525e238c1df804bd50e9640d7f2a7a50fc8c6
MannieJay <jhall02258@gmail.com>
2018-03-08 19:14:03 -0500

    completed character stat page

--- rpg/charactercreator/views.py
+++ rpg/charactercreator/views.py
@@ -2,14 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Character, Fighter, Mage, Cleric, Thief
 
+
 def index(request):
     return HttpResponse("Character Creator App!")
+
 
 def view_all_characters(request):
     characters = Character.objects.all()
     context = {'characters': characters}
     return render(request, 'characters/index.html', context)
 
-def view_character(request):
-    # TODO
-    pass
+
+def view_character(request, character_id):
+    character = Character.objects.get(pk=character_id)
+    context = {'character': character}
+    return render(request, 'character/index.html', context)
