10b7714277373407c2a8da95e90e395398f7ecb8
soycode <soycode@users.noreply.github.com>
2018-03-12 10:10:26 -0700

    Add basic working auth

--- rpg/rpg/settings.py
+++ rpg/rpg/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
 
-
 # Application definition
 
 INSTALLED_APPS = [
--- rpg/rpg/urls.py
+++ rpg/rpg/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 urlpatterns = [
     path('charactercreator/', include('charactercreator.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/login/', auth_views.login),
+    path('accounts/logout/', auth_views.logout),
 ]
