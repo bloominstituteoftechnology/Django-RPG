a3a5678bd60c183e01753e28ec8a5cd613059b9a
Tyler Sanford <tylersanford0311@gmail.com>
2018-03-13 11:24:51 -0500

    Auth working with FB and github

--- rpg/rpg/settings.py
+++ rpg/rpg/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.github',
+    'allauth.socialaccount.providers.facebook',
     # Our apps
     'armory',
     'charactercreator',
