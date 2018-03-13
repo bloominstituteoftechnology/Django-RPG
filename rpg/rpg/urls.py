072117de5d092b5940f894a8a7be973a2235d68e
CosmonautJones <travisj_jones@hotmail.com>
2018-03-13 12:33:30 -0500

    Still no github oath, but there is a REST api

--- rpg/rpg/urls.py
+++ rpg/rpg/urls.py
@@ -14,9 +14,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.contrib.auth.models import User
+from django.urls import include, path, re_path
+from rest_framework import routers, serializers, viewsets
+from charactercreator.api import CharacterViewSet
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'characters', CharacterViewSet)
 
 urlpatterns = [
     path('charactercreator/', include('charactercreator.urls')),
     path('admin/', admin.site.urls),
+    re_path(r'^accounts/', include('allauth.urls')),
+    re_path(r'^api/', include(router.urls)),
+    re_path(r'^api-auth/', include('rest_framework.urls')),
 ]
