010de3f71f6f7a42e21e711bb69124fbe616f3da
Tyler Sanford <tylersanford0311@gmail.com>
2018-03-13 12:00:33 -0500

    gitignore fix

--- rpg/rpg/settings.py
+++ rpg/rpg/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.github',
+    'allauth.socialaccount.providers.facebook',
     'rest_framework',
     # Our apps
     'armory',
