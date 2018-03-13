13039317107759a9dd72e257da95f513b433f6fd
soycode <soycode@users.noreply.github.com>
2018-03-13 08:47:34 -0700

    Remove outdated context processor import

--- rpg/rpg/settings.py
+++ rpg/rpg/settings.py
@@ -71,8 +71,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # Required by allauth template tags
-                'django.core.context_processors.request',
             ],
         },
     },
