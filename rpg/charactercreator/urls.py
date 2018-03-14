53e7f78debebe6eb5c7440f768d917ac5fa81b4c
Samuel Kim <SamuelKim@Owners-MacBook-Air.local>
2018-03-14 14:46:27 -0700

    added basic GraphQL query

--- rpg/charactercreator/urls.py
+++ rpg/charactercreator/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path
-
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('characters/', views.view_all_characters, name='characters'),
-    path('character/([0-9]{1})/', views.view_character, name='character')
+    path('character/([0-9]{1})/', views.view_character, name='character'),
 ]
--- rpg/charactercreator/views.py
+++ rpg/charactercreator/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from .models import Character, Fighter, Mage, Cleric, Thief
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 def index(request):
     return HttpResponse("Character Creator App!!!")
+
+@login_required
 def view_all_characters(request):
     characters = Character.objects.all()
     context = {'characters': characters}
