9afb3b55c75f02d82f4128428f0ede699e3920f5
Lorin Fields <lorin.fields@gmail.com>
2018-03-14 16:36:37 -0700

    feeling gitty

--- rpg/rpg/settings.py
+++ rpg/rpg/settings.py
@@ -27,15 +27,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    # Django packages
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+<<<<<<< HEAD
     'django.contrib.sites',    
     'django.contrib.staticfiles',
     'allauth',
@@ -43,6 +44,16 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.github',
     
     
+=======
+    'django.contrib.sites',
+    'django.contrib.staticfiles',
+    # 3rd party packages
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.github',
+    # Our apps
+>>>>>>> 13039317107759a9dd72e257da95f513b433f6fd
     'armory',
     'charactercreator',
 ]
@@ -74,6 +85,13 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = (
+    # Default backend -- used to login by username in Django admin
+    'django.contrib.auth.backends.ModelBackend',
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
 
 WSGI_APPLICATION = 'rpg.wsgi.application'
 
@@ -126,4 +144,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+<<<<<<< HEAD
+=======
+
+>>>>>>> 13039317107759a9dd72e257da95f513b433f6fd
 SITE_ID = 1
