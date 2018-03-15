0653e179911677db6ab75f9db05d327626f89b7c
MannieJay <jhall02258@gmail.com>
2018-03-15 15:17:50 -0400

    migrated to postgres, prepped for deployment

--- rpg/charactercreator/models.py
+++ rpg/charactercreator/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from armory.models import Item
+from rpg.armory.models import Item
+
 
 class Character(models.Model):
     """Base representation of RPG characters."""
@@ -12,27 +13,33 @@ class Character(models.Model):
     intelligence = models.IntegerField(default=1)
     dexterity = models.IntegerField(default=1)
     wisdom = models.IntegerField(default=1)
-    inventory = models.ManyToManyField(Item, verbose_name="Items the character has")
+    inventory = models.ManyToManyField(
+        Item, verbose_name="Items the character has")
+
 
 class Fighter(Character):
     """Martial warrior class good at hitting things."""
     using_shield = models.BooleanField(default=False)
     rage = models.IntegerField(default=100)
 
+
 class Mage(Character):
     """Arcane spellcasters with a familiar pet."""
     has_pet = models.BooleanField(default=True)
     mana = models.IntegerField(default=100)
+
 
 class Cleric(Character):
     """Mystical healers who can use shields."""
     using_shield = models.BooleanField(default=False)
     mana = models.IntegerField(default=100)
 
+
 class Thief(Character):
     """Sneaky rogues good at stealth and ranged attacks."""
     is_sneaking = models.BooleanField(default=False)
     energy = models.IntegerField(default=100)
+
 
 class Necromancer(Mage):
     """Spellcaster specialized in the arts of the undead."""
--- rpg/charactercreator/views.py
+++ rpg/charactercreator/views.py
@@ -4,19 +4,16 @@ from django.shortcuts import render
 from .models import Character, Fighter, Mage, Cleric, Thief
 
 
-@login_required
 def index(request):
     return HttpResponse("Character Creator App!")
 
 
-@login_required
 def view_all_characters(request):
     characters = Character.objects.all()
     context = {'characters': characters}
     return render(request, 'characters/index.html', context)
 
 
-@login_required
 def view_character(request, character_id):
     character = Character.objects.get(pk=character_id)
     try:
