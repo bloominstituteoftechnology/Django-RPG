894ba164c16cb0ed07ec458f113333f9c072a508
David Long <davdaarn@live.com>
2018-03-15 17:30:10 -0500

    copied from other dir

--- charactercreator/views.py
+++ charactercreator/views.py
@@ -3,14 +3,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Character, Fighter, Mage, Cleric, Thief
 
+
 def index(request):
     return HttpResponse("Character Creator App!")
+
 
 @login_required
 def view_all_characters(request):
     characters = Character.objects.all()
     context = {'characters': characters}
     return render(request, 'characters/index.html', context)
+
 
 def view_character(request):
     # TODO
--- rpg/urls.py
+++ rpg/urls.py
@@ -1,19 +1,13 @@
-"""rpg URL Configuration
+from django.conf.urls import include, url
+# from django.urls import path
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+admin.autodiscover()
+
+import hello.views
+
+
+# from django.contrib import admin
 from django.contrib.auth.models import User
 from django.urls import include, path, re_path
 from rest_framework import routers, serializers, viewsets
@@ -40,10 +34,15 @@ class UserViewSet(viewsets.ModelViewSet):
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'characters', CharacterViewSet)
+# Examples:
+# url(r'^$', 'gettingstarted.views.home', name='home'),
+# url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    path('charactercreator/', include('charactercreator.urls')),
+    # url(r'^$', hello.views.index, name='index'),
+    # url(r'^db', hello.views.db, name='db'),
     path('admin/', admin.site.urls),
+    path('charactercreator/', include('charactercreator.urls')),
     re_path(r'^accounts/', include('allauth.urls')),
     re_path(r'^api/', include(router.urls)),
     re_path(r'^api-auth/', include('rest_framework.urls')),
