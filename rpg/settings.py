37815e743e5064914fd2e87780e8fbb6e51c26a7
Tyler Sanford <tylersanford0311@gmail.com>
2018-03-15 16:41:14 -0500

    final heroku fixes

--- rpg/settings.py
+++ rpg/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+LOGIN_REDIRECT_URL = '/charactercreator/characters/'
+
 import os
 from decouple import config
 import dj_database_url
