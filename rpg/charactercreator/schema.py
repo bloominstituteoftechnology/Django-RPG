06edb3124ab4b68fe401a5af581285b2ffd1415d
Tyler Sanford <tylersanford0311@gmail.com>
2018-03-15 09:33:22 -0500

    Aarons EOD Change

--- rpg/charactercreator/schema.py
+++ rpg/charactercreator/schema.py
@@ -1,15 +1,21 @@
 from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 import graphene
 from .models import Character as CharacterModel
 
-class Character(DjangoObjectType):
+class CharacterNode(DjangoObjectType):
     class Meta:
         model = CharacterModel
+        interfaces = (graphene.relay.Node, )
+        filter_fields = {
+            'name': ['exact', 'icontains', 'istartswith'],
+            'level': ['exact', 'lt', 'gt'],
+        }
 
 class Query(graphene.ObjectType):
-    characters = graphene.List(Character)
+    all_characters = DjangoFilterConnectionField(CharacterNode)
 
-    def resolve_characters(self, info):
-        return CharacterModel.objects.all()
+    # def resolve_characters(self, info):
+    #     return CharacterModel.objects.all()
 
 schema = graphene.Schema(query=Query)
