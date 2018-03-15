15b8acdcca2c921277ebdb083227270a01c64d45
Dan Volosnikov <danvolosnikov@gmail.com>
2018-03-15 18:33:55 -0400

    added character view urls

--- rpg/charactercreator/urls.py
+++ rpg/charactercreator/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('characters/', views.view_all_characters, name='characters'),
+    path('character/<int:character_id>/', views.view_character, name='character'),
+    path('character/<int:character_id>/items', views.view_all_items, name='items'),
+    path('character/<int:character_id>/item/<int:item_id>', views.view_item, name='item'),
 ]
