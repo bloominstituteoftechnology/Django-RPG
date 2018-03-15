3573278b9ed7daccfa06aa2db92d768c20dd8dd4
Dan Volosnikov <danvolosnikov@gmail.com>
2018-03-15 19:33:46 -0400

    added item-details

---
+++ rpg/armory/api.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers, viewsets
+from .models import Item, Weapon
+
+class ItemSerializer(serializers.ModelSerializer):
+    is_weapon = serializers.BooleanField(source='weapon.is_weapon')
+
+    class Meta:
+        model = Item
+        fields = ('name', 'value', 'weight', 'is_weapon')
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
