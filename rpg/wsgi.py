60ca9a4ce5d4a58144f3186283af7cc84077cf25
soycode <soycode@users.noreply.github.com>
2018-03-15 10:11:52 -0700

    Correct settings path in wsgi.py

--- rpg/wsgi.py
+++ rpg/wsgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rpg.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
