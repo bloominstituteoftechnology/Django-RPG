d839b81412104fc639a6845eae4641cdf1408570
invegat <mark.c.oliver@gmail.com>
2018-03-15 17:37:29 -0400

    ElephantSQL remote database

--- rpg/rpg/settings.py
+++ rpg/rpg/settings.py
@@ -109,10 +109,20 @@ WSGI_APPLICATION = 'rpg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tspmqnqd',
+        'USER': 'tspmqnqd',
+        'PASSWORD': 'DUpHB8qa0PEAxnqXFBsLklj7FtSF8QaJ',
+        'HOST': 'baasu.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
 
