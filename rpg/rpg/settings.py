263299525ab2986c3355154f8c1bf7645c788f20
invegat <mark.c.oliver@gmail.com>
2018-03-15 19:34:10 -0400

    removed django_extensions

--- rpg/rpg/settings.py
+++ rpg/rpg/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.sites',
     'django.contrib.staticfiles',
-    'django_extensions',    
+    # 'django_extensions',    
     # 3rd party packages
     'allauth',
     'allauth.account',
