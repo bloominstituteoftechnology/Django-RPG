5bd3a0267c75c297db43ad06722629baf894171e
soycode <soycode@users.noreply.github.com>
2018-03-14 20:06:46 -0700

    Minimal working filter GraphQL queries on Characters

--- rpg/charactercreator/schema.py
+++ rpg/charactercreator/schema.py
@@ -1,15 +1,18 @@
 from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 import graphene
 from .models import Character as CharacterModel
 
-class Character(DjangoObjectType):
+class CharacterNode(DjangoObjectType):
     class Meta:
         model = CharacterModel
+        interfaces = (graphene.relay.Node, )
+        filter_fields = {
+            'name': ['exact', 'icontains', 'istartswith'],
+            'level': ['exact', 'lt', 'gt'],
+        }
 
 class Query(graphene.ObjectType):
-    characters = graphene.List(Character)
-
-    def resolve_characters(self, info):
-        return CharacterModel.objects.all()
+    all_characters = DjangoFilterConnectionField(CharacterNode)
 
 schema = graphene.Schema(query=Query)
