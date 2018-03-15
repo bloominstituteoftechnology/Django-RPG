8cc9344b7735185c6e17e27be49f02cfe76859ee
soycode <soycode@users.noreply.github.com>
2018-03-15 10:08:30 -0700

    Remove extra os.path.dirname from settings.py

--- rpg/settings.py
+++ rpg/settings.py
@@ -15,7 +15,7 @@ from decouple import config
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
