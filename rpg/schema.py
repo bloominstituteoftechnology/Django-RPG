f876fc080ddcd0a50d68092c3a35881a88306a60
Dan Volosnikov <danvolosnikov@gmail.com>
2018-03-16 15:14:17 -0400

    fixing graphql err

--- rpg/schema.py
+++ rpg/schema.py
@@ -1,7 +1,7 @@
 import graphene
 import rpg.charactercreator.schema
 
-class Query( rpg.charactercreator.schema.Query, graphene.ObjectType):
+class Query(rpg.charactercreator.schema.Query, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query)
