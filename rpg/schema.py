4c58665b5a26d74f47b6aa385bf81187d808a229
Dan Volosnikov <danvolosnikov@gmail.com>
2018-03-16 13:55:50 -0400

    graphql fix

--- rpg/schema.py
+++ rpg/schema.py
@@ -1,8 +1,7 @@
-# import graphene
-# import rpg.charactercreator.schema
-# import rpg.armory.schema
+import graphene
+import rpg.charactercreator.schema
 
-# class Query(rpg.armory.schema.Query, rpg.charactercreator.schema.Query, graphene.ObjectType):
-#     pass
+class Query( rpg.charactercreator.schema.Query, graphene.ObjectType):
+    pass
 
-# schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query)
