5b7bd207e5d697ee1ef570623b2c395a742fa26d
Mikisho <mikiye.82@gmail.com>
2018-03-16 12:49:29 -0400

    forheroku

--- rpg/charactercreator/schema.py
+++ rpg/charactercreator/schema.py
@@ -3,16 +3,19 @@ from graphene_django.filter import DjangoFilterConnectionField
 import graphene
 from .models import Character as CharacterModel
 
+
 class CharacterNode(DjangoObjectType):
-  class Meta:
-    model = CharacterModel
-    interfaces = (graphene.relay.Node,)
-    filter_fields = {
-      'name': ['exact', 'icontains', 'istartswith'],
-      'level': ['exact', 'it', 'gt'],
-    }
+    class Meta:
+        model = CharacterModel
+        interfaces = (graphene.relay.Node, )
+        filter_fields = {
+            'name': ['exact', 'icontains', 'istartswith'],
+            'level': ['exact', 'lt', 'gt'],
+        }
+
 
 class Query(graphene.ObjectType):
-  all_characters = DjangoFilterConnectionField(CharacterNode)
+    all_characters = DjangoFilterConnectionField(CharacterNode)
+
 
 schema = graphene.Schema(query=Query)
