2cf437799b3e27389f1ccf292a9697bc2fcb9fb1
Teresa Strout <qapla47@gmail.com>
2018-03-16 13:42:28 -0400

    character html

--- rpg/charactercreator/views.py
+++ rpg/charactercreator/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Character, Fighter, Mage, Cleric, Thief
+from django import template
 
 # everything that follows @login_required will hide the endpoint until a user logs in
 def index(request):
