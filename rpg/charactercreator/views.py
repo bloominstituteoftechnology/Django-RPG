049e757728b9336f280fc5c638a287884c6ae95a
MannieJay <jhall02258@gmail.com>
2018-03-16 18:12:14 -0400

    experimenting with vars in character.html

--- rpg/charactercreator/views.py
+++ rpg/charactercreator/views.py
@@ -31,7 +31,7 @@ def view_character(request, character_id):
                     charClass = character.cleric.__class__.__name__
                 except:
                     pass
-    charImage = f'https://s3.us-east-2.amazonaws.com/djangorpg/{charClass}.png'
+    charImage = https: // s3.us-east-2.amazonaws.com/djangorpg/{charClass}.png
     context = {'character': character,
                'charClass': charClass, 'charImage': charImage}
     return render(request, 'character/index.html', context)
