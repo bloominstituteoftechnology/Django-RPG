1fc28a6884208e1b832928b4142265f8fe46a4d0
CosmonautJones <travisj_jones@hotmail.com>
2018-03-15 19:00:09 -0500

    adding rpg to armory and charactercreator in setting

--- rpg/rpg/wsgi.py
+++ rpg/rpg/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rpg.rpg.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rpg.settings")
 
 print('GOT THIS FAR! In wsgi.py')
 
