1c1df17eec4bc04960e58f5618e4b79086afcdb8
Dan Volosnikov <danvolosnikov@gmail.com>
2018-03-16 13:32:49 -0400

    fixed items route

--- rpg/schema.py
+++ rpg/schema.py
@@ -1,7 +1,8 @@
 import graphene
 import rpg.charactercreator.schema
+import rpg.armory.schema
 
-class Query(rpg.charactercreator.schema.Query, graphene.ObjectType):
+class Query(rpg.armory.schema.Query, rpg.charactercreator.schema.Query, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query)
