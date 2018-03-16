86f12b43203ad78ccd35711c0d10e3b425827542
MannieJay <jhall02258@gmail.com>
2018-03-16 13:31:01 -0400

    added whitenoise to wsgi

--- rpg/wsgi.py
+++ rpg/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rpg.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
