78e25b96c687294ba546747b133b7dd1f733371e
Dan Volosnikov <danvolosnikov@gmail.com>
2018-03-16 16:05:01 -0400

    one more try with render

--- rpg/views.py
+++ rpg/views.py
@@ -1,6 +1,4 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 def index(request):
-    template = loader.get_template('main/index.html')
-    return HttpResponse(template.render(request))
+  return render(request, 'main/index.html')
