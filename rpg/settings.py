536af5f6de5be2c10baafe363426d28fb27d6dad
CosmonautJones <travisj_jones@hotmail.com>
2018-03-16 16:30:25 -0500

    destroying database and starting over

--- rpg/settings.py
+++ rpg/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 #ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG', cast=bool)
 
 # Application definition
 
