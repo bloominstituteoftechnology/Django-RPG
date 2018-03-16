29f09aa6d67748572bbb70ce26247c049f3a2566
Teresa Strout <qapla47@gmail.com>
2018-03-16 19:33:54 -0400

    Styling?

--- rpg/views.py
+++ rpg/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
-from .models import Character, Fighter, Mage, Cleric, Thief, Necromancer, Walker, Runner, Berzerker
 from django import template
 
 # everything that follows @login_required will hide the endpoint until a user logs in
