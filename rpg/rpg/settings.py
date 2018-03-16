a0846e1aa4984f9aa2e42b61337eb73ebd1c5a10
invegat <mark.c.oliver@gmail.com>
2018-03-15 20:15:18 -0400

    static changes

--- rpg/rpg/settings.py
+++ rpg/rpg/settings.py
@@ -21,20 +21,24 @@ from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'rgb/charactercreator/static')
+STATIC_URL = '/rgb/charactercreator/static/'
+STATICFILES_DIRS = (
+    # os.path.join(BASE_DIR, 'rgb/charactercreator/static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'sn_#zt!4p&gnj+@l_r#pwq^b)h@fv2%8@-$a=kbki1so=d7_ma'
+
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -132,16 +136,8 @@ DATABASES  = {
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'tspmqnqd',
-#         'USER': 'tspmqnqd',
-#         'PASSWORD': 'DUpHB8qa0PEAxnqXFBsLklj7FtSF8QaJ',
-#         'HOST': 'baasu.db.elephantsql.com',
-#         'PORT': '5432',
-#     }
-# }
+
+
 
 
 # Password validation
