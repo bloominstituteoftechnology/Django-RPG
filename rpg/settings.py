3e52b36a92357243d71aac3b25ed795f06ecff43
invegat <mark.c.oliver@gmail.com>
2018-03-19 16:56:24 -0400

    setup static

--- rpg/settings.py
+++ rpg/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/rgp/charactercreator/static')
-STATIC_URL = '/rgp/charactercreator/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/charactercreator/static')
+STATIC_URL = '/charactercreator/static/'
 STATICFILES_DIRS = (
-    # os.path.join(BASE_DIR, 'rgb/charactercreator/static'),
+    # os.path.join(BASE_DIR, 'charactercreator/static'),
 )
 
 # Quick-start development settings - unsuitable for production
@@ -89,7 +89,7 @@ ROOT_URLCONF = 'rpg.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
