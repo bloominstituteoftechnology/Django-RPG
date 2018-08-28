5c220836d1abcc4e993c9096274d9c7e99f7bb12
Sam-Park <sam.park.email@gmail.com>
2018-08-28 18:23:47 -0400

    queries complete

--- README.md
+++ README.md
@@ -13,11 +13,22 @@ You should familiarize yourself a bit with these classes and objects - read the 
 
 Your main goal is to write Python code that uses the Django ORM to answer:
 
-* [ ] How many total Characters are there?
+* [ ] How many total Characters are there? 302
+select count(*) from charactercreator_character;
 * [ ] How many of each specific subclass?
+select count(*) from charactercreator_cleric;
+  cleric = 75
+  thief = 51
+  mage = 108;
+  figher = 68;
+  necromancer = 11;
 * [ ] How many total Items?
+174
 * [ ] How many of the Items are weapons? How many are not?
+  weapons = 37;
+  other = 137;
 * [ ] On average, how many Items does each Character have?
+  2.92
 * [ ] On average, how many Weapons does each character have?
 
 You can experiment/execute your code using the Django shell. Please turn in a file `queries.py` in this repo with your code along with comments for your answers.
--- queries.py
+++ queries.py
@@ -1,35 +1,69 @@
 import sqlite3
 
-conn = sqlite3.connect('django.db.backends.sqlite3')
-c = conn.cursor()
-# sqlite_file = './mydatabase'
+db = sqlite3.connect('./mydatabase')
 
-# * [ ] How many total Characters are there? 302
+cur = db.cursor()
+
+# How many total characters
+total_characters = cur.execute('SELECT count(*) from charactercreator_character')
+total_characters_value = total_characters.fetchone()
+print(total_characters_value[0])
+#answer 302
+# How many total clerics
+cur.execute('SELECT count(*) from charactercreator_cleric')
+print(cur.fetchall())
+#answer 75
+# How many total thiefs
+cur.execute('SELECT count(*) from charactercreator_thief')
+print(cur.fetchall())
+#answer 51
+# How many total fighters
+cur.execute('SELECT count(*) from charactercreator_fighter')
+print(cur.fetchall())
+#answer 68
+# How many total mages
+cur.execute('SELECT count(*) from charactercreator_mage')
+print(cur.fetchall())
+#answer 108
+# How many total necromancers
+necro_data = cur.execute('SELECT count(*) from charactercreator_necromancer')
+necro_value = necro_data.fetchone()
+print(necro_value[0])
+#answer 11
+# How many total Items
+total_items = cur.execute('SELECT count(*) from armory_item')
+total_items_value = total_items.fetchone()
+print(total_items_value[0])
+#answer 174
+# How many of the items are weapons
+total_weapons = cur.execute('select count(*) from armory_weapon')
+total_weapons_value = total_weapons.fetchone()
+print(total_weapons_value[0])
+#answer 37
+# How many of the items are not weapons
+items_total = (total_items_value[0] - total_weapons_value[0])
+print(items_total)
+#answer 137
+# on average how many items does each character have?
+total_inv = cur.execute('select count(*) from charactercreator_character_inventory')
+total_inv_value = total_inv.fetchone()
+average_num_items = (total_inv_value[0] / total_characters_value[0])
+print(average_num_items)
+#answer 2.973...
+# On average, how many weapons does each character have?
+weap_in_inv = cur.execute('select count(item_id) from charactercreator_character_inventory where item_id between 138 and 174')
+total_weap_in_inv = weap_in_inv.fetchone()
+average_weap_per_char = (total_weap_in_inv[0] / total_characters_value[0])
+print(average_weap_per_char)
+#answer 0.67
+db.close()
+
+# * [ ] How many total Characters are there?
 # select count(*) from charactercreator_character;
 # * [ ] How many of each specific subclass?
-# select count(*) from charactercreator_cleric;
-#   cleric = 75
-#   thief = 51
-#   mage = 108;
-#   figher = 68;
-#   necromancer = 11;
 # * [ ] How many total Items?
-# 174
 # * [ ] How many of the Items are weapons? How many are not?
-#   weapons = 37;
-#   other = 137;
 # * [ ] On average, how many Items does each Character have?
-#   2.92
 # * [ ] On average, how many Weapons does each character have?
 
 
-
-SELECT
-count(*)
-FROM
-charactercreator_character;
-
-SELECT
-count(*)
-FROM
-charactercreator_cleric
