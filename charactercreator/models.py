79a9a3b154ed7985795c7f22fdecd5d1ccffa0a7
AmyShackles <amyshackles@gmail.com>
2018-08-28 15:46:43 -0700

    Queries.py includes total number of characters
    - Includes number of thieves
    - Includes number of fighters
    - Includes number of mages
    - Includes number of clerics
    - Includes number of necromancers
    - Includes number of items
    - Includes number of weapons

--- charactercreator/models.py
+++ charactercreator/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from armory.models import Item
 
+
 class Character(models.Model):
     """Base representation of RPG characters."""
+
     character_id = models.AutoField(primary_key=True)
     name = models.CharField("Character's name", max_length=30)
     level = models.IntegerField(default=0)
@@ -14,27 +16,40 @@ class Character(models.Model):
     wisdom = models.IntegerField(default=1)
     inventory = models.ManyToManyField(Item, verbose_name="Items the character has")
 
+
 class Fighter(Character):
     """Martial warrior class good at hitting things."""
+
     using_shield = models.BooleanField(default=False)
     rage = models.IntegerField(default=100)
 
+
 class Mage(Character):
     """Arcane spellcasters with a familiar pet."""
+
     has_pet = models.BooleanField(default=True)
     mana = models.IntegerField(default=100)
 
+
 class Cleric(Character):
     """Mystical healers who can use shields."""
+
     using_shield = models.BooleanField(default=False)
     mana = models.IntegerField(default=100)
 
+
 class Thief(Character):
     """Sneaky rogues good at stealth and ranged attacks."""
+
     is_sneaking = models.BooleanField(default=False)
     energy = models.IntegerField(default=100)
 
+    class Meta:
+        verbose_name_plural = "thieves"
+
+
 class Necromancer(Mage):
     """Spellcaster specialized in the arts of the undead."""
+
     # Charged talismans can be used to raise the dead!
     talisman_charged = models.BooleanField(default=True)
