0bd2bfb1efb49fc12e10bfb5907a10d7aa952e1f
Bradiowave <divebombwalker@gmail.com>
2018-08-28 14:19:52 -0700

    answered all MVP questions

--- queries.py
+++ queries.py
@@ -3,6 +3,7 @@
 >>> c = Character.objects.all()
 >>> len(c)
 302 #number of total Characters
+
 >>> from charactercreator.models import Fighter
 >>> fighters = Fighter.objects.all()
 >>> len(fighters)
@@ -20,3 +21,20 @@
 >>> necromancers = Necromancer.objects.all()
 >>> len(necromancers)
 11 #number of necromancers
+
+>>> from armory.models import Item, Weapon
+>>> items = Item.objects.all()
+>>> len(items)
+174 #total number of items
+
+>>> weapons = Weapon.objects.all()
+>>> len(weapons)
+37 #total number of weapons
+>>> len(items) - len(weapons)
+137 #number of items that are not weapons
+
+>>> len(items) / len(c)
+0.5761589403973509 #average number of items per character
+
+>>> len(weapons) / len(c)
+0.12251655629139073 #average number of weapons per character
