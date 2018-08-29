a2642e09bc6f068835b97a1b1d38e5ff431a5646
Danny Vail <danny_vail@outlook.com>
2018-08-29 18:27:41 -0400

    IMPLEMENT QUERIES FOR NUM WEAPONS AND NUM ITEMS NOT WEAPONS In queries.py - Implement Django ORM query to find total number of weapons - Implement Django ORM query to find total number of items that are not weapons

--- queries/queries.py
+++ queries/queries.py
@@ -39,3 +39,24 @@ count() returns the number of objects matching the QuerySet
 https://docs.djangoproject.com/en/2.1/ref/models/querysets/#count
 """
 num_items = Item.objects.count()
+
+
+"""
+How many of the items are weapons? How many are not?
+
+count() returns the number of objects matching the QuerySet
+https://docs.djangoproject.com/en/2.1/ref/models/querysets/#count
+
+filter() returns a new set that matches the given paramters
+https://docs.djangoproject.com/en/2.1/ref/models/querysets/#filter
+
+Field lookups can be used as a paramter in filter() (and exclude() and get())
+Field lookups correspond to the sql WHERE command
+https://docs.djangoproject.com/en/2.1/ref/models/querysets/#field-lookups
+
+The field lookup isnull checks if a field is or is not set to null:
+SELECT ... WHERE field IS NULL (or IS NOT NULL)
+https://docs.djangoproject.com/en/2.1/ref/models/querysets/#std:fieldlookup-isnull
+"""
+num_weapons = Weapon.objects.count()
+num_items_not_weapons = Item.objects.filter(weapon__isnull=True).count()
