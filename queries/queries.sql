81619ac9496cef69d0875ebb0a16b42fd21ea56a
Danny Vail <danny_vail@outlook.com>
2018-08-29 18:01:41 -0400

    IMPLEMENT SQL QUERIES TO FIND TOTAL NUMBER OF CHARACTERS FOR EACH CLASS In queries.sql - Implement SQL queries to find number of characters for each subclass

--- queries/queries.sql
+++ queries/queries.sql
@@ -2,3 +2,10 @@
 
 -- How many total Characters are there?
 SELECT COUNT(character_id) FROM charactercreator_character;
+
+-- How many of each specific subclass?
+SELECT COUNT(character_ptr_id) FROM charactercreator_fighter;
+SELECT COUNT(character_ptr_id) FROM charactercreator_mage;
+SELECT COUNT(character_ptr_id) FROM charactercreator_cleric;
+SELECT COUNT(character_ptr_id) FROM charactercreator_thief;
+SELECT COUNT(mage_ptr_id) FROM charactercreator_necromancer;
