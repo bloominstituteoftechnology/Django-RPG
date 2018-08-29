b774bb074abeaf33202b1a4d67ad127ed0027508
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-08-29 20:10:48 +0800

    Did the following:
    1. Created pipenv virtual environment: "pipenv --three"
    2. Installed Django.
    3. Installed "pipenv install dj-database-url".
    4. In admin.py, imported from .models the classes Character, etc.
    5. Registered the models in admin.py.

--- charactercreator/admin.py
+++ charactercreator/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Character, Fighter, Mage, Cleric, Thief, Necromancer
 
 # Register your models here.
+admin.site.register(Character)
+admin.site.register(Fighter)
+admin.site.register(Mage)
+admin.site.register(Cleric)
+admin.site.register(Thief)
+admin.site.register(Necromancer)
